edbbaaae0f73e2f1183a1663ad17958ecd6e667c
Mindy Borovsky <mindyborovsky@mindys-air.dhcp.wustl.edu>
2016-11-09 16:58:55 -0600

    Initial Commit

---
+++ ChallengeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChallengeMe
+//
+//  Created by Mindy Borovsky on 11/9/16.
+//  Copyright © 2016 Mindy Borovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
