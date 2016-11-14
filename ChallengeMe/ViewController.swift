8fa9509b9d9905acc60efa03013abd7a59133a7e
Daniel Mangum <danielmangum@wustl.edu>
2016-11-14 12:53:08 -0600

    - Bill was Added - Hi Bill

--- ChallengeMe/ViewController.swift
+++ ChallengeMe/ViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
 import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
     
+    var ref: FIRDatabaseReference!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let loginButton = FBSDKLoginButton()
-        loginButton.delegate = self
+        //loginButton.delegate = self
+        
+        ref = FIRDatabase.database().reference()
+        
+        self.ref.child("users").child("Users").setValue(["name": "Bill"])
+
+        
     }
 
     @IBAction func didPressLoginButton(_ sender: UIButton) {
