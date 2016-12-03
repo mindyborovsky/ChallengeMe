03644556902ec2f3a8fe9b658ad21d15b574879d
Mindy Borovsky <mindyborovsky@mindys-air.dhcp.wustl.edu>
2016-12-03 16:12:05 -0600

    complete events

--- ChallengeMe/userChallenge.swift
+++ ChallengeMe/userChallenge.swift
@@ -21,4 +21,27 @@ struct UserChallenge {
     
     var id: String
     
+    // TODO: always use this
+    func toDictionary() -> Dictionary<String,Any> {
+        var dict: Dictionary<String,Any> = ["name":""]
+        dict["name"] = self.name
+        dict["creator"] = self.creator
+        dict["status"] = self.status
+        dict["opponent"] = self.opponent
+        dict["id"] = self.id
+        return dict
+    }
+    
+    // TODO: always use this
+    func saveToFirebase(ref: FIRDatabaseReference, uid: String) {
+        ref.child("Users/\(uid)/Challenges/\(self.id)").setValue(self.toDictionary())
+    }
+    
+    
+    static func initWith(challenge: Challenge, userId: String) -> UserChallenge {
+        let creator = userId == challenge.creatorId
+        let opponent = creator ? challenge.opponentId : challenge.creatorId
+        return UserChallenge(creator: creator, name: challenge.name!, status: challenge.status!, opponent: opponent!, id: challenge.id!)
+        
+    }
 }
