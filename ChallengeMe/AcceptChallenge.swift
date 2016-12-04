5dc9040caee2a88fb2555edc925823b4e6e53914
Daniel Mangum <danielmangum@wustl.edu>
2016-12-03 19:31:58 -0600

    commit

--- ChallengeMe/AcceptChallenge.swift
+++ ChallengeMe/AcceptChallenge.swift
@@ -75,8 +75,19 @@ class AcceptChallenge: UIViewController {
         
 
             
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
         
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        tap.cancelsTouchesInView = false
         
+        view.addGestureRecognizer(tap)
+        
+    }
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
     func updateView() {
--- ChallengeMe/ChallengeInfoVC.swift
+++ ChallengeMe/ChallengeInfoVC.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseDatabase
+import FirebaseAuth
+import FirebaseStorage
 
 class ChallengeInfoVC: UIViewController {
     
+    @IBOutlet weak var creatorImage: UIImageView!
+    
+    @IBOutlet weak var opponentImage: UIImageView!
+    
     var currChallenge = Challenge()
+    
+    var ref: FIRDatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        ref = FIRDatabase.database().reference()
+        
+        ref.child("Users/\(currChallenge.creatorId!)").observeSingleEvent(of: .value, with: { snapshot in
+            
+            if !snapshot.exists() { return }
+            
+            
+            let dict: NSDictionary = snapshot.value as! NSDictionary
+            if let creatorPic = dict["Picture"] as? String {
+                DispatchQueue.global().async {
+                    let url = NSURL(string: creatorPic)
+                    let data = try? Data(contentsOf: url! as URL) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+                    DispatchQueue.main.async {
+                        self.creatorImage.image = UIImage(data: data!)
+                        // this is delayed by a cycle
+                        self.view.setNeedsDisplay()
+                        
+                    }
+                }
+            }
+        });
         
         self.title = currChallenge.name
         // Do any additional setup after loading the view.
